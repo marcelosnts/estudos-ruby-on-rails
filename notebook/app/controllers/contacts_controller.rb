53a575b4f2c8d9573c032efb1dacc4a1f15c3ebc
Marcelo <santosmarcelo1995@live.com>
2019-02-22 11:12:38 -0300

    Atualizacao notebook

--- notebook/app/controllers/contacts_controller.rb
+++ notebook/app/controllers/contacts_controller.rb
@@ -15,6 +15,7 @@ class ContactsController < ApplicationController
   # GET /contacts/new
   def new
     @contact = Contact.new
+    @contact.build_address
     kind_options
   end
 
@@ -75,6 +76,6 @@ class ContactsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def contact_params
-      params.require(:contact).permit(:name, :email, :kind_id, :rmk)
+      params.require(:contact).permit(:name, :email, :kind_id, :rmk, address_attributes: [ :street, :city, :state ])
     end
 end
